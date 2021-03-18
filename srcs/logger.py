2f4985f6e192d50cce171dc3dd13b3b604419ab6
Nikola Radakovic <radaknikolans@gmail.com>
2021-03-18 11:25:49 +0100

    Add new version of logger without color format
    Add new version of STLogger (new name) which can support different
    log levels as well different module, file and/or function logging.
    
    Issue: #3

---
+++ srcs/logger.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+# --------------------------------------------------------------
+# logger.py
+# --------------------------------------------------------------
+# Copyright (C) 2021 Nikola Radakovic
+# email: radaknikolans@gmail.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+""" Logger module for writhing messages to standard output
+"""
+
+# ---------------------------------------------------------------------
+# Imports
+# ---------------------------------------------------------------------
+import sys
+import argparse
+import logging as log
+
+# ---------------------------------------------------------------------
+# Authorship information
+# ---------------------------------------------------------------------
+__author__     = "Nikola Radakovic"
+__copyright__  = "Copyright 2021, The Logger V2 Project"
+__license__    = "GPL"
+__version__    = "2.0.0"
+__maintainer__ = "Nikola Radakovic"
+__email__      = "radaknikolans@gmail.com"
+__status__     = "Development"
+
+
+# ---------------------------------------------------------------------
+# Public class
+# ---------------------------------------------------------------------
+class STLogger(log.Logger):
+    """
+    Implementation of logger class which is used to print logs from
+    different modules, files, and functions with colored output or
+    without it (several formats supported).
+    """
+    _MODULES = ({})
+    _DEFAULT_NAME = "STLogger"
+    _FORMAT = (
+        "%(asctime)s - "
+        "%(name)s - "
+        "%(levelname)s: "
+        "%(message)s"
+    )
+
+    def __init__(self, default_name=_DEFAULT_NAME):
+        """
+        Constructor of the logger which will set default name in main
+        logging class
+        :param default_name: string name which will be used for all
+                             logs which don't set name explicitly.
+        """
+        super().__init__(name=default_name)
+
+    @staticmethod
+    def create_logger(module=None, level=log.INFO, c_format=None):
+        """
+        Static class which creates custom logger.
+        :param module: string name of the module
+        :param level: int level of supported messages
+        :param c_format: tuple object representing format for messages
+        :return: Logger objects which can be used to print messages
+        """
+
+        name = STLogger._DEFAULT_NAME
+        if module is not None:
+            STLogger._MODULES.update({"module": module, "level": level})
+            name = module
+
+        logger = log.getLogger(name)
+        if not logger.handlers:
+            stream = log.StreamHandler()
+            formatter = log.Formatter(c_format if c_format is not None
+                                      else STLogger._FORMAT)
+            stream.setFormatter(log.Formatter(formatter))
+            logger.addHandler(stream)
+            logger.setLevel(level)
+
+        if logger.level != level:
+            logger.setLevel(level)
+
+        return logger
+
+
+# ---------------------------------------------------------------------
+# Public functions
+# ---------------------------------------------------------------------
+def authorship():
+    """
+    Function for printing Authorship information to the STDOUT without
+    any formatting.
+    """
+    print(f'Author:     {__author__}')
+    print(f'Copyright:  {__copyright__}')
+    print(f'License:    {__license__}')
+    print(f'Version:    {__version__}')
+    print(f'Maintainer: {__maintainer__}')
+    print(f'E-Mail:     {__email__}')
+    print(f'Status:     {__status__}')
+
+
+def parse_argv(argv):
+    """
+    Parse command line arguments
+    :return: parsing object
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--authorship_information",
+                        dest="auth_info", action="store_false",
+                        default=True)
+    parser.add_argument("-v", "--version", dest="version",
+                        action="store_true", default=False)
+    return parser.parse_args(argv)
+
+
+def main(argv=None):
+    """
+    Main execution function
+    :return: 0 in case of success, otherwise error code
+    """
+    args = parse_argv(argv if argv is not None else sys.argv[1:])
+
+    if args.auth_info:
+        authorship()
+
+    if args.version:
+        print(f'{__version__}')
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
