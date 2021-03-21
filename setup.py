00542671ce70dcd7fe765e3d48431db149994453
Nikola Radakovic <radaknikolans@gmail.com>
2021-03-21 09:46:44 +0100

    Change project structure and add setup.py
    Change project structure and add setup.py which is used to build
    whl package. The tests are also removed since there are no tests.
    
    Issue: #9

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='stlogger',
+    version='0.0.1',
+    license="MIT License",
+    author="Nikola Radakovic",
+    description="A custom colored logger",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
---
+++ stlogger/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from stlogger.stlogger import STLogger, \
+    NOLOG, DEBUG, INFO, WARNING, ERROR, CRITICAL
+
+__all__ = ["STLogger",
+           "NOLOG",
+           "DEBUG",
+           "INFO",
+           "WARNING",
+           "ERROR",
+           "CRITICAL"]
