8188a6b37094dd0ec74beffcc1c657a0269e71a4
Nikola Radakovic <35235060+nradakovic@users.noreply.github.com>
2021-03-26 01:44:43 +0100

    Set correct email address
    Set correct email address

--- stlogging/stlogging.py
+++ stlogging/stlogging.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # --------------------------------------------------------------
 # Copyright (C) 2021 Nikola Radakovic
-# email: radaknikolans@gmail.com
+# email: radaknikolans@yahoo.com
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
